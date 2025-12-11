9ecbcfc8e299e44d2b6b4b6fa7fd7dc9a7383b6b
DomiSkyX <140248230+DomiSkyX@users.noreply.github.com>
2025-12-11 16:02:19 +0100

    Initial Commit

---
+++ components.js
@@ -0,0 +1,7 @@
+fetch("components/header.html")
+  .then(r => r.text())
+  .then(html => document.querySelector("#header").innerHTML = html);
+
+fetch("components/footer.html")
+  .then(r => r.text())
+  .then(html => document.querySelector("#footer").innerHTML = html);
