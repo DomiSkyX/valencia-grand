2c4cef2b6308bdb04403d5eb8688b8e0c0f22bb1
DomiSkyX <140248230+DomiSkyX@users.noreply.github.com>
2025-12-12 21:25:18 +0100

    Made database memberlist

--- script.js
+++ script.js
@@ -15,3 +15,29 @@ document.querySelectorAll('.member-status').forEach(status => {
         }
     });
 });
+
+
+let { data: members, error } = await supabase
+  .from('member-list')
+  .select('name, id, rank, status');
+
+if (error) {
+    console.error(error);
+}
+
+const container = document.querySelector('.member-list');
+
+// alle Member in HTML umwandeln
+members.forEach(member => {
+    const div = document.createElement('div');
+    div.classList.add('member');
+
+    div.innerHTML = `
+        <div class="member-name">${member.name}</div>
+        <div class="member-id">${member.id}</div>
+        <div class="member-rank">${member.rank}</div>
+        <div class="member-status" data-status="${member.status.toLowerCase()}"></div>
+    `;
+
+    container.appendChild(div);
+});
